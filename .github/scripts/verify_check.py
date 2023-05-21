223078b1c436f24adbd6ffd26d8e411a02c71355
David <87927264+Rexicon226@users.noreply.github.com>
2023-05-20 22:25:03 -0700

    test #4

--- .github/scripts/verify_check.py
+++ .github/scripts/verify_check.py
@@ -4,10 +4,17 @@ import os
 PATH = '/home/runner/work/'
 
 swinfo_amount = 0
+swinfo_path = ""
+
+class ResolutionError(Exception):
+    pass
 
 for root, subFolder, files in os.walk(PATH):
     for item in files:
         if item == "swinfo.json" :
             swinfo_amount += 1
-
-print(swinfo_amount)
+            if swinfo_amount > 1:
+                raise ResolutionError("You have more than 1 'swinfo.info' file in your project, please resolve this")
+            swinfo_path = os.path.join(root, subFolder)
+    
+            
