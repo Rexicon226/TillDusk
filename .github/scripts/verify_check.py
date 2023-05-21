680734283085497026993f291950fbe856b19d5f
David <87927264+Rexicon226@users.noreply.github.com>
2023-05-20 22:14:08 -0700

    test #2

--- .github/scripts/verify_check.py
+++ .github/scripts/verify_check.py
@@ -1,1 +1,13 @@
-print("test")
+import json
+import os
+
+PATH = '/home/runner/work/'
+
+swinfo_amount = 0
+
+for root, subFolder, files in os.walk(PATH):
+    for item in files:
+        if item == "swinfo.json" :
+            swinfo_amount += 1
+
+print(swinfo_amount)
